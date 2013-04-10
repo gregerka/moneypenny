a5ff309e8105a27b0f3800abad2dff7fb573941c
Kathrin Greger <kathringreger@localhost.(none)>
2013-04-10 11:25:02 +0200

    add settings + basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  http_basic_authenticate_with :name => Settings.basic_auth.user, :password => Settings.basic_auth.password
 end
